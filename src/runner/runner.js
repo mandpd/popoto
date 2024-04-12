d0da491246cc2eb16cc37e98303e872ced826046
Mark Smith <mark@perfectfourth.com>
2024-04-12 13:09:26 -0700

    modified to query via oast api server

--- src/runner/runner.js
+++ src/runner/runner.js
@@ -1,43 +1,85 @@
-import logger from "../logger/logger";
+import logger from "../logger/logger"
 
 /**
  * runner module.
  * @module runner
  */
 
-var runner = {};
+var runner = {}
 
 runner.createSession = function () {
-    if (runner.DRIVER !== undefined) {
-        return runner.DRIVER.session({defaultAccessMode: "READ"})
-    } else {
-        throw new Error("popoto.runner.DRIVER must be defined");
-    }
-};
-
+  if (runner.DRIVER !== undefined) {
+    return runner.DRIVER.session({ defaultAccessMode: "READ" })
+  } else {
+    throw new Error("popoto.runner.DRIVER must be defined")
+  }
+}
 runner.run = function (statements) {
-    logger.info("STATEMENTS:" + JSON.stringify(statements));
-    var session = runner.createSession();
-
-    return session.readTransaction(function (transaction) {
-        return Promise.all(
-            statements.statements.map(function (s) {
-                return transaction.run({text: s.statement, parameters: s.parameters});
-            })
-        )
-    })
-        .finally(function () {
-            session.close();
+  logger.info("STATEMENTS:" + JSON.stringify(statements))
+  try {
+    return Promise.all(
+      statements.statements.map(async function (s) {
+        const response = await fetch("http://localhost:3001/oastapi/dev/njq", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            text: s.statement,
+            parameters: s.parameters,
+          }),
         })
-};
 
-runner.toObject = function (results) {
-    return results.map(function (rs) {
-        return rs.records.map(function (r) {
-            return r.toObject();
-        })
-    })
+        const data = await response.json()
+        console.log(`query data: ${JSON.stringify(data)}`)
+
+        return data
+      })
+    )
+  } catch (error) {
+    console.error(`Error: ${error}`)
+  }
 }
 
+// runner.run = function (statements) {
+//   logger.info("STATEMENTS:" + JSON.stringify(statements))
+//   var session = runner.createSession()
 
-export default runner;
+//   return session
+//     .readTransaction(function (transaction) {
+//       return Promise.all(
+//         statements.statements.map(function (s) {
+//           return transaction.run({
+//             text: s.statement,
+//             parameters: s.parameters,
+//           })
+//         })
+//       )
+//     })
+//     .finally(function () {
+//       session.close()
+//     })
+// }
+
+runner.toObject = function (results) {
+  return results.map(function (rs) {
+    return rs.records.map(function (r) {
+      const obj = {}
+      r.keys.forEach((key, i) => {
+        obj[key] = r._fields[i]
+      })
+      return obj
+    })
+  })
+}
+
+// runner.toObject = function (results) {
+//   console.log(`results: ${JSON.stringify(results)}`)
+//   return results.map(function (rs) {
+//     return rs.records.map(function (r) {
+//       return r.toObject()
+//     })
+//   })
+// }
+
+export default runner
